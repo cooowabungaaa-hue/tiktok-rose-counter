765a9e2df6cf860d17c62a2622ea0a8109e0acf9
cooowabungaaa-hue <cooowabungaaa@gmail.com>
2026-02-15 13:37:50 +0900

    Add stop script

---
+++ stop.bat
@@ -0,0 +1,11 @@
+@echo off
+echo ---------------------------------------------------
+echo Stopping TikTok Rose Counter...
+echo ---------------------------------------------------
+echo.
+taskkill /F /IM node.exe
+echo.
+echo ---------------------------------------------------
+echo All processes stopped. You can now run start.bat.
+echo ---------------------------------------------------
+pause
